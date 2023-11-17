4c8a62b2668901fcd7a7684a9602a4b229c6e498
Pedrooliveira100 <pedrosilva88pr@gmail.com>
2023-11-17 14:40:43 -0300

    aula de javascript
    aula sobre javascript

---
+++ Aula01/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("demo").innerHTML = "Café";
+
+function aumentar(){
+    document.getElementById("demo").style.fontSize = "35px";
+}
+function diminuir(){
+    document.getElementById("demo").style.fontSize = "20px";
+}
